1dc302fdf53c69714bfcc102bdf8461c1d4d0148
Ruslan <81382673+ffrusik@users.noreply.github.com>
2024-03-20 19:34:34 +0000

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     if (start == 1) {
         datalogger.log(datalogger.createCV("Glasses: ", water))
         basic.clearScreen()
         increment_glass()
-        basic.showString("You drank " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + water)))))))))))))) + " times", 100)
+        basic.showString("You drank " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + water))))))))))))))) + " times", 100)
     } else if (enter_gender == 0 && start == 0) {
         age += 1
     } else if (enter_gender == 1) {
@@ -11,55 +12,59 @@ input.onButtonPressed(Button.A, function () {
         gender = "male"
         start = 1
     }
+    
 })
-function increment_glass () {
+function increment_glass() {
+    
     water += 1
     reminder = 0
 }
-input.onPinPressed(TouchPin.P2, function () {
+
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
     food_and_glasses_total()
 })
-function increment_food () {
+function increment_food() {
+    
     food += 1
     reminder_food = 0
 }
-function food_and_glasses_total () {
+
+function food_and_glasses_total() {
     if (start == 1) {
         basic.clearScreen()
-        basic.showString("In total you drank " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + water)))))))))))))) + " times", 100)
-basic.showString("In total you ate " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + food)))))))))))))) + " times", 100)
+        basic.showString("In total you drank " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + water))))))))))))))) + " times", 100)
+        basic.showString("In total you ate " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + food))))))))))))))) + " times", 100)
     }
+    
 }
-input.onButtonPressed(Button.AB, function () {
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     if (start == 1) {
-        led.plotBarGraph(
-        water,
-        100
-        )
+        led.plotBarGraph(water, 100)
         serial.writeValue("water", water)
-        led.plotBarGraph(
-        food,
-        100
-        )
+        led.plotBarGraph(food, 100)
         serial.writeValue("food", food)
     }
+    
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     if (start == 1) {
         datalogger.log(datalogger.createCV("Meals: ", food))
         basic.clearScreen()
         increment_food()
-        basic.showString("You ate " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + food)))))))))))))) + " times", 150)
+        basic.showString("You ate " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + food))))))))))))))) + " times", 150)
     } else if (start == 0 && enter_gender == 0) {
         basic.clearScreen()
-        basic.showString("Your age: " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + age)))))))))))))) + "", 100)
-basic.showString("Enter your gender: (male - A, female - B)", 100)
-enter_gender = 1
+        basic.showString("Your age: " + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + ("" + age))))))))))))))) + "", 100)
+        basic.showString("Enter your gender: (male - A, female - B)", 100)
+        enter_gender = 1
     } else if (enter_gender == 1) {
         basic.clearScreen()
         gender = "female"
         start = 1
     }
+    
 })
 let time = 0
 let reminder_food = 0
@@ -75,21 +80,25 @@ age = 0
 food = 0
 water = 0
 reminder = 0
-loops.everyInterval(1000, function () {
+loops.everyInterval(1000, function on_every_interval() {
+    
     if (start == 1) {
         reminder += 1
         reminder_food += 1
         time += 1
     }
+    
 })
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     if (reminder >= 3600) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.showString("It's time to drink water", 150)
-reminder = 0
+        reminder = 0
     } else {
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
+    
     if (reminder_food >= 14400) {
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.showString("It's time to have a meal")
@@ -97,6 +106,7 @@ reminder = 0
     } else {
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
+    
     if (time >= 57600) {
         if (gender == "male") {
             if (age <= 1) {
@@ -107,6 +117,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age > 1 && age <= 3) {
                 if (water < 3) {
                     basic.showString("You should drink more")
@@ -115,6 +126,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 4 && age <= 8) {
                 if (water < 4) {
                     basic.showString("You should drink more")
@@ -123,6 +135,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 9 && age <= 13) {
                 if (water < 5) {
                     basic.showString("You should drink more")
@@ -131,6 +144,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 14 && age <= 18) {
                 if (water < 7) {
                     basic.showString("You should drink more")
@@ -139,6 +153,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 19) {
                 if (water < 9) {
                     basic.showString("You should drink more")
@@ -147,7 +162,9 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             }
+            
         } else if (gender == "female") {
             if (age <= 1) {
                 if (water < 1) {
@@ -157,6 +174,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age > 1 && age <= 3) {
                 if (water < 3) {
                     basic.showString("You should drink more")
@@ -165,6 +183,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 4 && age <= 8) {
                 if (water < 4) {
                     basic.showString("You should drink more")
@@ -173,6 +192,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 9 && age <= 13) {
                 if (water < 5) {
                     basic.showString("You should drink more")
@@ -181,6 +201,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 14 && age <= 18) {
                 if (water < 6) {
                     basic.showString("You should drink more")
@@ -189,6 +210,7 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             } else if (age >= 19) {
                 if (water < 7) {
                     basic.showString("You should drink more")
@@ -197,8 +219,11 @@ reminder = 0
                 } else {
                     basic.showString("You should drink less")
                 }
+                
             }
+            
         }
+        
         if (food < 5) {
             basic.showString("You did not eat enough today")
         } else if (food <= 6) {
@@ -206,5 +231,7 @@ reminder = 0
         } else {
             basic.showString("You should consider eating less")
         }
+        
     }
+    
 })
